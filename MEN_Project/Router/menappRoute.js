b662e85c1abea60bd2c583677fa4081056766478
Sandeep Gupta <guptasandeep188@gmail.com>
2021-08-03 07:29:14 +0530

    Delete menappRoute.js

--- MEN_Project/Router/menappRoute.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-console.log("hello");
-router.get('/', (req, res) => {
-    res.send('GEt Request');
-});
-
-module.exports = router;
